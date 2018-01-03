cc627f51fef31d8c318df1f1c71cd6d31fdbae12
Monica Tamaru Jones <mntamaru@gmail.com>
2018-01-03 17:29:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber === NaN) {
+  if (cardNumber === undefined) {
     console.log("Sorry, we don't have a credit card on file for you.")
  
   }
