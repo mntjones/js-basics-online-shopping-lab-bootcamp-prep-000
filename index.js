0f9dfb5e02daef22290101e9cb7d378fea130405
Monica Tamaru Jones <mntamaru@gmail.com>
2018-01-03 17:28:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -76,11 +76,18 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber === null) {
+    console.log("Sorry, we don't have a credit card on file for you.")
+ 
+  }
+  else{
+    var cost = total()
+    console.log("Your total cost is $" + cost + ", which will be charged to the card " + cardNumber + ".")
+    cart = []
+  }
 }
 
 addToCart("banana")
 addToCart("peach")
 addToCart("bread")
-
-removeFromCart("peanut")
+placeOrder()
